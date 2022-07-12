45237930ee94a6dc017c8dfda768d93045af2c30
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-12 23:01:54 +0200

    add css style

--- Milestone 1.php
+++ Milestone 1.php
@@ -4,7 +4,7 @@ Html, CSS, VueJS, axios, PHP
 Prima Milestone:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 
-<?php 
+<?php
 
 include "discs_db.php";
 
@@ -12,6 +12,7 @@ include "discs_db.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,28 +20,40 @@ include "discs_db.php";
     <title>Milestone 1</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
-    <div class="container py-5">
-        <div class="row row-cols-6 gap-4">
-            <?php 
-                foreach ($discList as $disc) {
-            ?>
-
-            <div class="card mx-2 p-3">
-                <img src="<?php echo $disc["poster"] ?>" alt="">
-                <div class="card-body text-center">
-                    <h4 class="card-title"><?php echo $disc["title"] ?></h4>
-                    <span><?php echo $disc["author"] ?></span><br>
-                    <span><?php echo $disc["year"] ?></span>
-                </div>
-            </div>
-
-            <?php } ?>
+    <header>
+        <div class="logo">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 167.5 167.5" class="spotify-logo">
+                <title>Spotify</title>
+                <path fill="#33DF6A" d="M83.7 0C37.5 0 0 37.5 0 83.7c0 46.3 37.5 83.7 83.7 83.7 46.3 0 83.7-37.5 83.7-83.7S130 0 83.7 0zM122 120.8c-1.4 2.5-4.6 3.2-7 1.7-19.8-12-44.5-14.7-73.7-8-2.8.5-5.6-1.2-6.2-4-.2-2.8 1.5-5.6 4-6.2 32-7.3 59.6-4.2 81.6 9.3 2.6 1.5 3.4 4.7 1.8 7.2zM132.5 98c-2 3-6 4-9 2.2-22.5-14-56.8-18-83.4-9.8-3.2 1-7-1-8-4.3s1-7 4.6-8c30.4-9 68.2-4.5 94 11 3 2 4 6 2 9zm1-23.8c-27-16-71.6-17.5-97.4-9.7-4 1.3-8.2-1-9.5-5.2-1.3-4 1-8.5 5.2-9.8 29.6-9 78.8-7.2 109.8 11.2 3.7 2.2 5 7 2.7 10.7-2 3.8-7 5-10.6 2.8z" />
+            </svg>
         </div>
-    </div>
+    </header>
+
+    <main>
+        <div class="container py-5">
+            <div class="row row-cols-6 gap-4">
+                <?php
+                foreach ($discList as $disc) {
+                ?>
+
+                    <div class="card mx-2 p-3 text-white">
+                        <img src="<?php echo $disc["poster"] ?>" alt="">
+                        <div class="my-card-body pt-3 text-center">
+                            <h4 class="card-title"><?php echo $disc["title"] ?></h4>
+                            <span class="text-secondary"><?php echo $disc["author"] ?></span><br>
+                            <span class="text-secondary"><?php echo $disc["year"] ?></span>
+                        </div>
+                    </div>
+
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 
 </body>
+
 </html>
--- Milestone 2.php
+++ Milestone 2.php
@@ -14,22 +14,34 @@ Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, a
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
     <div id="app">
-        <div class="container py-5">
-            <div class="row row-cols-6 gap-4">
-                <div class="card mx-2 p-3" v-for="disc in listaDischi">
-                    <img :src="disc.poster" alt="">
-                    <div class="text-center pt-3">
-                        <h4>{{ disc.title }}</h4>
-                        <span>{{ disc.author }}</span><br>
-                        <span>{{ disc.year }}</span>
+        <header>
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 167.5 167.5" class="spotify-logo">
+                    <title>Spotify</title>
+                    <path fill="#33DF6A" d="M83.7 0C37.5 0 0 37.5 0 83.7c0 46.3 37.5 83.7 83.7 83.7 46.3 0 83.7-37.5 83.7-83.7S130 0 83.7 0zM122 120.8c-1.4 2.5-4.6 3.2-7 1.7-19.8-12-44.5-14.7-73.7-8-2.8.5-5.6-1.2-6.2-4-.2-2.8 1.5-5.6 4-6.2 32-7.3 59.6-4.2 81.6 9.3 2.6 1.5 3.4 4.7 1.8 7.2zM132.5 98c-2 3-6 4-9 2.2-22.5-14-56.8-18-83.4-9.8-3.2 1-7-1-8-4.3s1-7 4.6-8c30.4-9 68.2-4.5 94 11 3 2 4 6 2 9zm1-23.8c-27-16-71.6-17.5-97.4-9.7-4 1.3-8.2-1-9.5-5.2-1.3-4 1-8.5 5.2-9.8 29.6-9 78.8-7.2 109.8 11.2 3.7 2.2 5 7 2.7 10.7-2 3.8-7 5-10.6 2.8z" />
+                </svg>
+            </div>
+        </header>
+
+        <main>
+            <div class="container py-5">
+                <div class="row row-cols-6 gap-4">
+                    <div class="card mx-2 p-3 text-white" v-for="disc in listaDischi">
+                        <img :src="disc.poster" alt="">
+                        <div class="text-center pt-3">
+                            <h4 class="card-title">{{ disc.title }}</h4>
+                            <span class="text-secondary">{{ disc.author }}</span><br>
+                            <span class="text-secondary">{{ disc.year }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
 
     <script src="vue.js"></script>
